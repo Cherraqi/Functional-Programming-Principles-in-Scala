b29dc05f38721b405a37360e3763b057940e9fad
cherraqi <cherraqi7@gmail.com>
2018-11-05 16:49:25 +0100

    first commit

---
+++ src/main/scala/Main.scala
@@ -0,0 +1,71 @@
+
+
+object  Main {
+  def main(args: Array[String]) {
+    /** *
+      * ********************* Test Pascal's Traingle Exercise ***********************
+      */
+    println("Pascal's Triangle")
+    val a = scala.io.StdIn.readInt()
+    for (row <- 0 to a) {
+      for (col <- 0 to row)
+        print(pascal(col, row) + " ")
+      println()
+    }
+    /** *
+      * ********************* Test Balance Exercise ***********************
+      */
+    var paranthesis = "(234(3(2)s)d))10" //The String
+    println(balance(paranthesis.toList))
+
+    /** *
+      * ********************* Test Counting Change Exercice  ***********************
+      */
+
+  }
+
+  /** *
+    * ********************* Pascal's Traingle Exercise ***********************
+    */
+  def pascal(c: Int, r: Int): Int = {
+    if (c == r || c <= 0) 1
+    else pascal(c, r - 1) + pascal(c - 1, r - 1)
+  }
+
+
+  /** *
+    * ********************************** Balance Exercise ************************
+    */
+
+
+  def balance(chars: List[Char]): Boolean = {
+    def balances(chars: List[Char], num: Int): Boolean = {
+      (chars,num) match {
+        case (Nil,num) => return num == 0
+        case (c :: tail,num) =>
+          if(c == '(') return balances(tail,num+1)
+          if(c == ')') return balances(tail,num-1)
+          else return balances(tail,num)
+      }
+    }
+    balances(chars,0)
+  }
+
+
+
+
+  /** *
+    * ********************************** Counting Change Exercise************************
+    */
+
+
+  def countChange(money: Int, coins: List[Int]): Int = {
+    if (coins.isEmpty) 0  /*if (coins.isEmpty) if (money == 0) 1 else 0*/
+    else if (money - coins.head == 0) 1
+    else if (money - coins.head < 0) 0
+    else countChange(money - coins.head, coins) + countChange(money, coins.tail)
+  }
+
+
+
+}
